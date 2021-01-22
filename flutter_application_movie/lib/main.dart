dc6c280c7be4434a7f75acd5161bdf75d5322ff7
Agustin Ferreira <71300843+agustinn1233@users.noreply.github.com>
2021-01-22 14:26:01 -0300

    Flutter App Movie

---
+++ flutter_application_movie/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_application_movie/src/pages/home_page.dart';
+import 'package:flutter_application_movie/src/pages/pelicula_detalle.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Películas',
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+        'detalle': (BuildContext context) => PeliculaDetalle(),
+      },
+    );
+  }
+}
